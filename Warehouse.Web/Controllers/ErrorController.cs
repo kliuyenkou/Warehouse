ca30261afb27a5d6f131c9f122638ecc9a23041c
kliuyenkou <klyuenkov@gmail.com>
2017-01-27 10:26:18 +0300

    Handle 404 and 500 errors.

---
+++ Warehouse.Web/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Web.Mvc;
+
+namespace Warehouse.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = (int)HttpStatusCode.NotFound;
+            return View("NotFound");
+        }
+        public ActionResult ServerError()
+        {
+            Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return View("Error");
+        }
+    }
+}
